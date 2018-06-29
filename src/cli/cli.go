db336771d1f0db2f3f5bf315064525e27d01f5f3
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-06-29 14:35:57 +0300

    update cli printUsage() method

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -10,14 +10,14 @@ type CLI struct{}
 
 func (cli *CLI) printUsage() {
 	fmt.Println("Usage:")
-	fmt.Print("  createblockchain\n    -address string\n\tThe address to send genesis block reward to\n\n")
-	fmt.Print("  createwallet\n\tGenerates a new key-pair and saves it into the wallet file\n\n")
-	fmt.Print("  getbalance\n    -address string\n\tThe address to get balance for\n\n")
-	fmt.Print("  listaddresses\n\tLists all addresses from the wallet file\n\n")
-	fmt.Print("  printchain\n\tPrint all the blocks of the blockchain\n\n")
-	fmt.Print("  reindexutxo\n\tRebuilds the UTXO set\n\n")
-	fmt.Print("  send\n    -from string\n\tSource wallet address\n    -to string\n\tDestination wallet address\n    -amount int\n\tAmount to send\n    -mine\n\tMine on the same node\n\n")
-	fmt.Print("  startnode\n    -miner string\n\tStart a node with ID specified in NODE_ID env. var. -miner enables mining\n\n")
+	fmt.Println("  count")
+	fmt.Println("    -f\n\tset file(s) to count lines")
+	fmt.Println("    -d\n\tset folder(s) to count lines")
+	fmt.Println("    -e\n\tset file(s) and(or) folder(s) to exclude from counting lines")
+	fmt.Println("    -json\n\twrite result to json file")
+	fmt.Println("    -xml\n\twrite result to xml file")
+	fmt.Print("    -yaml\n\twrite result to yaml file\n\n")
+	fmt.Print("  help\n\tRead usage\n\n")
 }
 
 func (cli *CLI) validateArgs() {
@@ -32,9 +32,9 @@ func (cli *CLI) Run() {
 //	f := countCmd.String("f", "", "set file(s) to count lines")
 //	d := countCmd.String("d", "", "set folder(s) to count lines")
 //	e := countCmd.String("e", "", "set file(s) and(or) folder(s) to exclude from counting lines")
-//	j := countCmd.Bool("j", false, "write result to json file")
-//	x := countCmd.Bool("x", false, "write result to xml file")
-//	y := countCmd.Bool("y", false, "write result to yaml file")
+//	j := countCmd.Bool("json", false, "write result to json file")
+//	x := countCmd.Bool("xml", false, "write result to xml file")
+//	y := countCmd.Bool("yaml", false, "write result to yaml file")
 
 	switch os.Args[1] {
 	case "count":
