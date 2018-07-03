039970c57445afa212eba3dc5c41afa097b9b410
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-07-03 22:12:20 +0300

    small changes

--- src/parser/vars.go
+++ src/parser/vars.go
@@ -6,78 +6,67 @@ type Enum int
 
 const (
 	IsSingleComment Enum = iota
-	IsMultiComment
-	IsBlank
 	IsCode
+	IsBlank
+	IsMultiComment
 	emptyString     = ""
 )
 
 var ExcludeList []string
 
 var (
-	noneComment   = models.Comment{emptyString, emptyString}
-	clangSComment = models.Comment{"//", "\n"}
-	clangMComment = models.Comment{"/*", "*/"}
-	shSComment    = models.Comment{"#", "\n"}
-	xmlComment    = models.Comment{"<!--", "-->"}
-	sqlSComment   = models.Comment{"--", "\n"}
-	agdaMComment  = models.Comment{"{-", "-}"}
-	aspSComment   = models.Comment{"'", "\n"}
-	mlMComment    = models.Comment{"(*", "*)"}
-	lispMComment  = models.Comment{"|#", "#|"}
+	noneComment    = models.Comment{emptyString, emptyString}
+	shSComment     = singleComment("#")
+	aspSComment    = singleComment("'")
+	clangSComment  = singleComment("//")
+	sqlSComment    = singleComment("--")
+	erlangSComment = singleComment("%")
+	clangMComment  = models.Comment{"/*", "*/"}
+	mlMComment     = models.Comment{"(*", "*)"}
+	agdaMComment   = models.Comment{"{-", "-}"}
+	lispMComment   = models.Comment{"|#", "#|"}
+	xmlComment     = models.Comment{"<!--", "-->"}
 )
 
 var (
 	languageData = map[string]models.AvailableLang{
-		"as":     cStyle("ActionScript"),
-		"c":      cStyle("C"),
-		"cpp":    cStyle("C++"),
-		"cs":     cStyle("C#"),
-		"ccpph":  cStyle("C/C++ Header"),
-		"go":     cStyle("Go"),
-		"groovy": cStyle("Groovy"),
-		"java":   cStyle("Java"),
-		"js":     cStyle("JavaScript"),
-		"json":   cStyle("JSON"),
-		"kt":     cStyle("Kotlin"),
-		"m":      cStyle("Objective-C"),
-		"mm":     cStyle("Objective-C"),
-		"php":    cStyle("PHP"),
-		"sass":   cStyle("Sass"),
-		"swift":  cStyle("Swift"),
-		"cfc":    cStyle("ColdFusionScript"),
-		"css":    cStyle("CSS"),
-		"cu":     cStyle("CUDA"),
-		"cuh":    cStyle("CUDA Header"),
-		"d":      cStyle("D"),
-		"dart":   cStyle("Dart"),
-		"dts":    cStyle("DeviceTree"),
-		"vert":   cStyle("GLSL"),
-		"jai":    cStyle("Jai"),
-		"jsx":    cStyle("Jsx"),
-		"less":   cStyle("Less"),
-		"lds":    cStyle("LinkerScript"),
-		"qcl":    cStyle("Qcl"),
-		"qml":    cStyle("QML"),
-		"rs":     cStyle("Rust"),
-		"scala":  cStyle("Scala"),
-		"styl":   cStyle("Stylus"),
-		"ts":     cStyle("TypeScript"),
-		"tsx":    cStyle("Typescript JSX"),
-		"uc":     cStyle("UnrealScript"),
-		"y":      cStyle("Yacc"),
-
-		"txt":  noComments("Plain text"),
-		"hex":  noComments("Hex"),
-		"ihex": noComments("Intel Hex"),
-		"rst":  noComments("reStructuredText"),
-
-		"html":   htmlStyle("HTML"),
-		"xml":    htmlStyle("XML"),
-		"polly":  htmlStyle("Polly"),
-		"cshtml": htmlStyle("Razor"),
-		"rhtml":  htmlStyle("RubyHtml"),
-
+		"as":       cStyle("ActionScript"),
+		"c":        cStyle("C"),
+		"cpp":      cStyle("C++"),
+		"cs":       cStyle("C#"),
+		"ccpph":    cStyle("C/C++ Header"),
+		"go":       cStyle("Go"),
+		"groovy":   cStyle("Groovy"),
+		"java":     cStyle("Java"),
+		"js":       cStyle("JavaScript"),
+		"json":     cStyle("JSON"),
+		"kt":       cStyle("Kotlin"),
+		"m":        cStyle("Objective-C"),
+		"mm":       cStyle("Objective-C"),
+		"php":      cStyle("PHP"),
+		"sass":     cStyle("Sass"),
+		"swift":    cStyle("Swift"),
+		"cfc":      cStyle("ColdFusionScript"),
+		"css":      cStyle("CSS"),
+		"cu":       cStyle("CUDA"),
+		"cuh":      cStyle("CUDA Header"),
+		"d":        cStyle("D"),
+		"dart":     cStyle("Dart"),
+		"dts":      cStyle("DeviceTree"),
+		"vert":     cStyle("GLSL"),
+		"jai":      cStyle("Jai"),
+		"jsx":      cStyle("Jsx"),
+		"less":     cStyle("Less"),
+		"lds":      cStyle("LinkerScript"),
+		"qcl":      cStyle("Qcl"),
+		"qml":      cStyle("QML"),
+		"rs":       cStyle("Rust"),
+		"scala":    cStyle("Scala"),
+		"styl":     cStyle("Stylus"),
+		"ts":       cStyle("TypeScript"),
+		"tsx":      cStyle("Typescript JSX"),
+		"uc":       cStyle("UnrealScript"),
+		"y":        cStyle("Yacc"),
 		"makefile": shStyle("Makefile"),
 		"r":        shStyle("R"),
 		"yml":      shStyle("YAML"),
@@ -89,15 +78,21 @@ var (
 		"sls":      shStyle("SaltStack"),
 		"tcl":      shStyle("Tcl"),
 		"toml":     shStyle("Toml"),
-
-		"v":   mlStyle("Coq"),
-		"ml":  mlStyle("OCaml"),
-		"sml": mlStyle("SML"),
-		"wl":  mlStyle("Wolfram"),
-
-		"oz": prologStyle("Oz"),
-		"p":  prologStyle("Prolog"),
-
+		"v":        mlStyle("Coq"),
+		"ml":       mlStyle("OCaml"),
+		"sml":      mlStyle("SML"),
+		"wl":       mlStyle("Wolfram"),
+		"html":     htmlStyle("HTML"),
+		"xml":      htmlStyle("XML"),
+		"polly":    htmlStyle("Polly"),
+		"cshtml":   htmlStyle("Razor"),
+		"rhtml":    htmlStyle("RubyHtml"),
+		"txt":      noComments("Plain text"),
+		"hex":      noComments("Hex"),
+		"ihex":     noComments("Intel Hex"),
+		"rst":      noComments("reStructuredText"),
+		"oz":       prologStyle("Oz"),
+		"p":        prologStyle("Prolog"),
 		"md":       language("Markdown", singleComment("[//]: # "), xmlComment),
 		"pas":      language("Pascal", models.Comment{"{", "}"}, models.Comment{"{*", "*}"}),
 		"pl":       language("Perl", shSComment, models.Comment{"=begin", "=cut"}),
@@ -113,7 +108,7 @@ var (
 		"coffee":   language("CoffeeScript", shSComment, models.Comment{"###", "###"}),
 		"fs":       language("F#", clangSComment, mlMComment),
 		"hs":       language("Haskell", sqlSComment, agdaMComment),
-		"erl":      language("Erlang", singleComment("%"), noneComment),
+		"erl":      language("Erlang", erlangSComment, noneComment),
 		"forth":    language("Forth", singleComment("\\"), models.Comment{"(", ")"}),
 		"f":        language("FORTRAN Legacy", singleComment("!"), noneComment),
 		"f03":      language("FORTRAN Modern", singleComment("!"), noneComment),
@@ -130,6 +125,6 @@ var (
 		"zig":      language("Zig", clangSComment, noneComment),
 		"arr":      language("Pyret", shSComment, lispMComment),
 		"tf":       language("Terraform", shSComment, clangMComment),
-		"tex":      language("TeX", singleComment("%"), noneComment),
+		"tex":      language("TeX", erlangSComment, noneComment),
 	}
 )
